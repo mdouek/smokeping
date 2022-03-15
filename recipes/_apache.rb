071c280dd1df97161e447fed9752b60712e278fa
mdouek <91295776+mdouek@users.noreply.github.com>
2022-03-15 13:58:24 -0700

    apache2

--- recipes/_apache.rb
+++ recipes/_apache.rb
@@ -16,7 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-apache_module 'cgi'
+apache2_module 'cgi'
 
 file '/etc/smokeping/apache2.config' do
   action :delete
